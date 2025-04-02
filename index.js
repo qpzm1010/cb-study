c0730db89152fa444a9d849605033f5a09d996fc
Hyunjun Yang <sw4019@naver.com>
2025-04-02 23:21:38 +0900

    Create index.js

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/health', (req, res) => {
+  res.status(200).json({'status': 'ok'});
+});
+
+app.get('/time', (req, res) => {
+    const now = new Date();
+    res.json({ time: now.toISOString() });
+  });
+
+app.listen(port, () => {
+  console.log(`Server listening at http://localhost:${port}`);
+});
